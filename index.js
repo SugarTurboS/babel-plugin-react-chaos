a48bddef9d98d1bc34f8ef4f8be3fdde38afd2d9
ForeverPx <1315592944@qq.com>
2020-08-14 17:47:58 +0800

    [modify]add chaos level

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ module.exports = function(babel){
     let t = babel.types;
     return {
         visitor: {
-            ImportDeclaration(path){
+            ImportDeclaration(path, state){
+                let chaosLevel = state.opts.chaosLevel ? state.opts.chaosLevel : 5;
                 const updateParamNameVisitor = {
                     ExportDefaultDeclaration(path) {
                         var dName = path.node.declaration.name;
                         if(dName){
                             const funcId = t.identifier('withChaos');
                             const func = t.callExpression(funcId,[path.node.declaration,
-                                t.numericLiteral(1), t.stringLiteral(dName + 'Chaos Error')]);
+                                t.numericLiteral(chaosLevel), t.stringLiteral(dName + 'Chaos Error')]);
                             const defaults = t.exportDefaultDeclaration(func);
                             path.replaceWith(defaults);
                         }
